3bffa8e3b6114835f20be14f9bce339099b50234
Agustín Carignano <agustin.carignano@gmail.com>
2023-06-28 09:22:01 +0200

    eliminate Navigate import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./features/searchPhotos/searchPhotosSlice.js";
 import SearchPhotosList from "./features/searchPhotos/SearchPhotosList";
 import MyCollectionList from "./features/myCollection/MyCollectionList";
